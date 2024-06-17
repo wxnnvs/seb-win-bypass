32151affbb42a4fe804074238cb769c874569761
JJorne-JDEVJ <159343253+JJorne-JDEVJ@users.noreply.github.com>
2024-06-17 17:41:30 +0200

    Grinning from ear to ear hehehehhehehe

--- SafeExamBrowser.Configuration/ConfigurationData/DataValues.cs
+++ SafeExamBrowser.Configuration/ConfigurationData/DataValues.cs
@@ -100,61 +100,15 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 		{
 			var settings = new AppSettings();
 
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "AA_v3.exe", OriginalName = "AA_v3.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "AeroAdmin.exe", OriginalName = "AeroAdmin.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "beamyourscreen-host.exe", OriginalName = "beamyourscreen-host.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "CamPlay.exe", OriginalName = "CamPlay.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Camtasia.exe", OriginalName = "Camtasia.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "CamtasiaStudio.exe", OriginalName = "CamtasiaStudio.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Camtasia_Studio.exe", OriginalName = "Camtasia_Studio.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "CamRecorder.exe", OriginalName = "CamRecorder.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "CamtasiaUtl.exe", OriginalName = "CamtasiaUtl.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "chromoting.exe", OriginalName = "chromoting.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "CiscoCollabHost.exe", OriginalName = "CiscoCollabHost.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "CiscoWebExStart.exe", OriginalName = "CiscoWebExStart.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Discord.exe", OriginalName = "Discord.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Element.exe", OriginalName = "Element.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "g2mcomm.exe", OriginalName = "g2mcomm.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "g2mlauncher.exe", OriginalName = "g2mlauncher.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "g2mstart.exe", OriginalName = "g2mstart.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "GotoMeetingWinStore.exe", OriginalName = "GotoMeetingWinStore.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "join.me.exe", OriginalName = "join.me.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "join.me.sentinel.exe", OriginalName = "join.me.sentinel.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Microsoft.Media.player.exe", OriginalName = "Microsoft.Media.player.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Mikogo-host.exe", OriginalName = "Mikogo-host.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "MS-teams.exe", OriginalName = "MS-Teams.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "obs32.exe", OriginalName = "obs32.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "obs64.exe", OriginalName = "obs64.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "PCMonitorSrv.exe", OriginalName = "PCMonitorSrv.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "pcmontask.exe", OriginalName = "pcmontask.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "ptoneclk.exe", OriginalName = "ptoneclk.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "RemotePCDesktop.exe", OriginalName = "RemotePCDesktop.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "remoting_host.exe", OriginalName = "remoting_host.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "RPCService.exe", OriginalName = "RPCService.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "RPCSuite.exe", OriginalName = "RPCSuite.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Skype.exe", OriginalName = "Skype.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "SkypeApp.exe", OriginalName = "SkypeApp.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "SkypeHost.exe", OriginalName = "SkypeHost.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "slack.exe", OriginalName = "slack.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "spotify.exe", OriginalName = "spotify.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "SRServer.exe", OriginalName = "SRServer.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "strwinclt.exe", OriginalName = "strwinclt.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Teams.exe", OriginalName = "Teams.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "TeamViewer.exe", OriginalName = "TeamViewer.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Telegram.exe", OriginalName = "Telegram.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "VLC.exe", OriginalName = "VLC.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "vncserver.exe", OriginalName = "vncserver.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "vncviewer.exe", OriginalName = "vncviewer.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "vncserverui.exe", OriginalName = "vncserverui.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "webexmta.exe", OriginalName = "webexmta.exe" });
-			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Zoom.exe", OriginalName = "Zoom.exe" });
+			settings.Applications.Blacklist.Add(new BlacklistApplication { ExecutableName = "Nothing_hehe", OriginalName = "somthings wrong euhhhh" });
 
-			settings.Browser.AdditionalWindow.AllowAddressBar = false;
+
+			settings.Browser.AdditionalWindow.AllowAddressBar = true;
 			settings.Browser.AdditionalWindow.AllowBackwardNavigation = true;
-			settings.Browser.AdditionalWindow.AllowDeveloperConsole = false;
+			settings.Browser.AdditionalWindow.AllowDeveloperConsole = true;
 			settings.Browser.AdditionalWindow.AllowForwardNavigation = true;
 			settings.Browser.AdditionalWindow.AllowReloading = true;
-			settings.Browser.AdditionalWindow.FullScreenMode = false;
+			settings.Browser.AdditionalWindow.FullScreenMode = true;
 			settings.Browser.AdditionalWindow.Position = WindowPosition.Right;
 			settings.Browser.AdditionalWindow.RelativeHeight = 100;
 			settings.Browser.AdditionalWindow.RelativeWidth = 50;
@@ -169,16 +123,16 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.Browser.AllowPageZoom = true;
 			settings.Browser.AllowPdfReader = true;
 			settings.Browser.AllowPdfReaderToolbar = false;
-			settings.Browser.AllowPrint = false;
+			settings.Browser.AllowPrint = true;
 			settings.Browser.AllowUploads = true;
 			settings.Browser.DeleteCacheOnShutdown = true;
 			settings.Browser.DeleteCookiesOnShutdown = true;
 			settings.Browser.DeleteCookiesOnStartup = true;
 			settings.Browser.EnableBrowser = true;
-			settings.Browser.MainWindow.AllowAddressBar = false;
-			settings.Browser.MainWindow.AllowBackwardNavigation = false;
-			settings.Browser.MainWindow.AllowDeveloperConsole = false;
-			settings.Browser.MainWindow.AllowForwardNavigation = false;
+			settings.Browser.MainWindow.AllowAddressBar = true;
+			settings.Browser.MainWindow.AllowBackwardNavigation = true;
+			settings.Browser.MainWindow.AllowDeveloperConsole = true;
+			settings.Browser.MainWindow.AllowForwardNavigation = true;
 			settings.Browser.MainWindow.AllowReloading = true;
 			settings.Browser.MainWindow.FullScreenMode = false;
 			settings.Browser.MainWindow.RelativeHeight = 100;
@@ -205,10 +159,10 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.Display.IgnoreError = false;
 			settings.Display.InternalDisplayOnly = false;
 
-			settings.Keyboard.AllowAltEsc = false;
-			settings.Keyboard.AllowAltF4 = false;
+			settings.Keyboard.AllowAltEsc = true;
+			settings.Keyboard.AllowAltF4 = true;
 			settings.Keyboard.AllowAltTab = true;
-			settings.Keyboard.AllowCtrlEsc = false;
+			settings.Keyboard.AllowCtrlEsc = true;
 			settings.Keyboard.AllowEsc = true;
 			settings.Keyboard.AllowF1 = true;
 			settings.Keyboard.AllowF2 = true;
@@ -222,12 +176,12 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.Keyboard.AllowF10 = true;
 			settings.Keyboard.AllowF11 = true;
 			settings.Keyboard.AllowF12 = true;
-			settings.Keyboard.AllowPrintScreen = false;
-			settings.Keyboard.AllowSystemKey = false;
+			settings.Keyboard.AllowPrintScreen = true;
+			settings.Keyboard.AllowSystemKey = true;
 
 			settings.LogLevel = LogLevel.Debug;
 
-			settings.Mouse.AllowMiddleButton = false;
+			settings.Mouse.AllowMiddleButton = true;
 			settings.Mouse.AllowRightButton = true;
 
 			settings.PowerSupply.ChargeThresholdCritical = 0.1;
@@ -255,7 +209,7 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.Security.KioskMode = KioskMode.CreateNewDesktop;
 			settings.Security.VerifyCursorConfiguration = true;
 			settings.Security.VerifySessionIntegrity = true;
-			settings.Security.VirtualMachinePolicy = VirtualMachinePolicy.Deny;
+			settings.Security.VirtualMachinePolicy = VirtualMachinePolicy.Allow;
 
 			settings.Server.PingInterval = 1000;
 			settings.Server.RequestAttemptInterval = 2000;
@@ -263,18 +217,18 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.Server.RequestTimeout = 30000;
 			settings.Server.PerformFallback = false;
 
-			settings.Service.DisableChromeNotifications = true;
-			settings.Service.DisableEaseOfAccessOptions = true;
+			settings.Service.DisableChromeNotifications = false;
+			settings.Service.DisableEaseOfAccessOptions = false;
 			settings.Service.DisableFindPrinter = true;
-			settings.Service.DisableNetworkOptions = true;
-			settings.Service.DisablePasswordChange = true;
-			settings.Service.DisablePowerOptions = true;
-			settings.Service.DisableRemoteConnections = true;
-			settings.Service.DisableSignout = true;
-			settings.Service.DisableTaskManager = true;
-			settings.Service.DisableUserLock = true;
-			settings.Service.DisableUserSwitch = true;
-			settings.Service.DisableVmwareOverlay = true;
+			settings.Service.DisableNetworkOptions = false;
+			settings.Service.DisablePasswordChange = false;
+			settings.Service.DisablePowerOptions = false;
+			settings.Service.DisableRemoteConnections = false;
+			settings.Service.DisableSignout = false;
+			settings.Service.DisableTaskManager = false;
+			settings.Service.DisableUserLock = false;
+			settings.Service.DisableUserSwitch = false;
+			settings.Service.DisableVmwareOverlay = false;
 			settings.Service.DisableWindowsUpdate = true;
 			settings.Service.IgnoreService = true;
 			settings.Service.Policy = ServicePolicy.Mandatory;
@@ -289,7 +243,7 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.UserInterface.ActionCenter.ShowApplicationLog = false;
 			settings.UserInterface.ActionCenter.ShowClock = true;
 			settings.UserInterface.ActionCenter.ShowKeyboardLayout = true;
-			settings.UserInterface.ActionCenter.ShowNetwork = false;
+			settings.UserInterface.ActionCenter.ShowNetwork = true;
 			settings.UserInterface.LockScreen.BackgroundColor = "#ff0000";
 			settings.UserInterface.Mode = UserInterfaceMode.Desktop;
 			settings.UserInterface.Taskbar.EnableTaskbar = true;
@@ -297,7 +251,7 @@ namespace SafeExamBrowser.Configuration.ConfigurationData
 			settings.UserInterface.Taskbar.ShowApplicationLog = false;
 			settings.UserInterface.Taskbar.ShowClock = true;
 			settings.UserInterface.Taskbar.ShowKeyboardLayout = true;
-			settings.UserInterface.Taskbar.ShowNetwork = false;
+			settings.UserInterface.Taskbar.ShowNetwork = true;
 
 			return settings;
 		}
